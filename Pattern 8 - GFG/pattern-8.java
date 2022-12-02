6421818fc8014ef4927011112288f275200ff71f
Prudhvi-Teja <87839700+Prudhvi-Teja@users.noreply.github.com>
2022-12-02 08:50:44 +0530

    Added solution - LeetHub

---
+++ Pattern 8 - GFG/pattern-8.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj=new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        
+        for(int i = n-1; i>=0 ; i--){
+            
+            int toc = 2*i+1;
+            int soc = n+i-toc;
+            
+            for(int j=0; j< soc; j++)
+                System.out.print(" ");
+                
+            for(int j=0; j< toc ; j++ )
+                System.out.print("*");
+                
+           System.out.println();
+            
+            
+            
+            
+        }
+        
+        
+    }
+}
