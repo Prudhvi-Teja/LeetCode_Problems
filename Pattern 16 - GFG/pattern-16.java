447ab54d3dd659a3c0e65a7ee102b9c26d26621e
Prudhvi-Teja <87839700+Prudhvi-Teja@users.noreply.github.com>
2022-12-21 20:56:02 +0530

    Added solution - LeetHub

---
+++ Pattern 16 - GFG/pattern-16.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        
+        char[] arr = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+            'Q','R','S','T','U','V','W','X','Y','Z'};
+        
+        for(int i =0 ; i<n ; i++){
+            
+            for(int j = 0; j<=i;j++)
+                System.out.print(arr[i]);
+            
+            System.out.println();
+            
+        }
+        
+        
+    }
+}
