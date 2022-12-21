4fba02fe26bc64a1b3d2054b62505b6aa40f5de0
Prudhvi-Teja <87839700+Prudhvi-Teja@users.noreply.github.com>
2022-12-21 23:08:40 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (80.05%) - LeetHub

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -0,0 +1,21 @@
+class Solution {
+ public char findKthBit(int n, int k) {
+        boolean flip = false;
+        while (k != 1) {
+            int base = floorTwo(k);
+            if (base == k) {
+                return flip ? '0' : '1';
+            }
+            flip = !flip;
+            k = base - (k - base);
+        }
+        return flip ? '1' : '0';
+    }
+
+    private int floorTwo(int k) {
+        while ((k & (k - 1)) > 0) {
+            k &= k - 1;
+        }
+        return k;
+    }
+}
