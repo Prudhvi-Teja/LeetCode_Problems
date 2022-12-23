78cd767390f222f7f9e2e51f73ac936887fddfe7
Prudhvi-Teja <87839700+Prudhvi-Teja@users.noreply.github.com>
2022-12-23 22:10:03 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (80.87%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -5,34 +5,37 @@ class Solution {
     */
     public int[][] flipAndInvertImage(int[][] image) {
         
-        // Calculation length it is nxn matrix single variable is enough
-        int n = image.length;
-        
         // First step is to reverse column in each row by swapping 
         
-        for(int i =0 ; i< n; i++){
+        for(int i =0 ; i< image.length; i++){
             
-            for(int j = 0 ; j<n/2; j++){
+             // Calculation length it is nxn matrix single variable is enough
+             int n = image.length-1;
+             int s = 0;
+            
+            while(s<=n){
                 
-                int temp = image[i][j];
-                image[i][j] = image[i][n-1-j];
-                image[i][n-1-j] = temp;
+                int temp = image[i][s]^1;
+                image[i][s] = image[i][n]^1;
+                image[i][n] = temp;
+                s++;
+                n--;
                 
             }
             
         }
         
-        // Replace the 0's with 1 and viceversa
+//         // Replace the 0's with 1 and viceversa
         
-        for(int i = 0; i< n; i++){
-            for(int j=0; j<n; j++){
-                if(image[i][j]==0)
-                    image[i][j] = 1;
-                else
-                    image[i][j] = 0;
-            }
+//         for(int i = 0; i< n; i++){
+//             for(int j=0; j<n; j++){
+//                 if(image[i][j]==0)
+//                     image[i][j] = 1;
+//                 else
+//                     image[i][j] = 0;
+//             }
                 
-        }
+//         }
         
         return image;
         
